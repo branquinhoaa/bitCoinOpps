d6787cbbdcb5eff6a0e21929aa421d43c9227e71
branquinhoaa <amanda.alves15@gmail..com>
2017-03-03 10:44:17 -0300

    include method to get the lowest ask and highest bid from kraken data

--- opportunity-api/app/controllers/opportunities_controller.rb
+++ opportunity-api/app/controllers/opportunities_controller.rb
@@ -4,6 +4,7 @@ class OpportunitiesController < ApplicationController
   # GET /todos
   def index
     @opportunities = Opportunity.all
+lowest_ask
     json_response(@opportunities)
   end
 
@@ -31,6 +32,32 @@ class OpportunitiesController < ApplicationController
   end
 
   private
+
+  def get_kraken_data
+    client = KrakenClient.load
+    # data will have value, amount, id - in this order
+    data = client.public.order_book('XXBTZUSD') # this is the code for USD BTC
+  end
+
+  def get_btce_data
+  end
+  
+  def lowest_ask
+    kraken =  get_kraken_data
+    kraken_asks = kraken['XXBTZUSD']['asks'] #here I get all the asks
+    lowest_kraken = kraken_asks.min_by{ |order| order[0] } #here I choose the best kraken ask (cheaper)
+    byebug
+    Ask.new(exchange: 'two', value: 255) #build the ask object with the lowest ask value
+  end
+
+  def largest_bid
+    kraken = get_kraken_data
+    kraken_bids = kraken['XXBTZUSD']['bids'] #here I get all bids
+    larger_bid = kraken_bids.max_by{ |order| order[0] } #here I choose the best kraken bid (more expensive)
+    Bid.new(exchange: 'one', value: 255) #build the bid object with the highest bid value
+  end
+
+
   def set_opportunity
     @opportunity = Opportunity.find(params[:id])
   end
