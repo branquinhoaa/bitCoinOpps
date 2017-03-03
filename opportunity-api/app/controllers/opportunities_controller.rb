a11ef5a7826dec4bd6165ce689bb050d9ba772b3
branquinhoaa <amanda.alves15@gmail..com>
2017-03-03 11:59:14 -0300

    find the best opportunity

--- opportunity-api/app/controllers/opportunities_controller.rb
+++ opportunity-api/app/controllers/opportunities_controller.rb
@@ -4,15 +4,14 @@ class OpportunitiesController < ApplicationController
   # GET /todos
   def index
     @opportunities = Opportunity.all
-lowest_ask
     json_response(@opportunities)
   end
 
   # POST /todos
   def create
     @opportunity = Opportunity.new
-    @opportunity.bid=Bid.new(exchange: 'one', value: 255)
-    @opportunity.ask=Ask.new(exchange: 'two', value: 255)
+    @opportunity.bid = largest_bid
+    @opportunity.ask = lowest_ask
     if @opportunity.save!
       json_response(@opportunity, :created)
     else
@@ -35,26 +34,42 @@ lowest_ask
 
   def get_kraken_data
     client = KrakenClient.load
-    # data will have value, amount, id - in this order
     data = client.public.order_book('XXBTZUSD') # this is the code for USD BTC
   end
 
   def get_btce_data
+    require 'btce'
+    Btce::Depth.new "btc_usd"   #This method provides the information about active orders on the pair.
   end
-  
+
   def lowest_ask
     kraken =  get_kraken_data
-    kraken_asks = kraken['XXBTZUSD']['asks'] #here I get all the asks
-    lowest_kraken = kraken_asks.min_by{ |order| order[0] } #here I choose the best kraken ask (cheaper)
-    byebug
-    Ask.new(exchange: 'two', value: 255) #build the ask object with the lowest ask value
+    btce = get_btce_data
+
+    lowest_kraken = kraken['XXBTZUSD']['asks'].min_by{ |order| order[0] } #return value, amount, id
+    lowest_btce = btce.json["btc_usd"]["asks"].min_by{ |order| order[0] } #return value, amount
+
+    #build the ask object with the lowest ask value:
+    if lowest_kraken[0] < lowest_btce[0]
+      Ask.new(exchange: 'kraken', value: lowest_kraken[0])
+    else
+      Ask.new(exchange: 'btce', value: lowest_btce[0])
+    end
   end
 
   def largest_bid
     kraken = get_kraken_data
-    kraken_bids = kraken['XXBTZUSD']['bids'] #here I get all bids
-    larger_bid = kraken_bids.max_by{ |order| order[0] } #here I choose the best kraken bid (more expensive)
-    Bid.new(exchange: 'one', value: 255) #build the bid object with the highest bid value
+    btce = get_btce_data
+
+    higher_kraken = kraken['XXBTZUSD']['bids'].max_by{ |order| order[0] } #here I choose the best kraken bid (more expensive)
+    higher_btce = btce.json["btc_usd"]["bids"].max_by{ |order| order[0] } #return value, amount
+
+    #build the bid object with the highest bid value
+    if higher_kraken[0] > higher_btce[0]
+      Bid.new(exchange: 'kraken', value: higher_kraken[0])
+    else
+      Bid.new(exchange: 'btce', value: higher_btce[0])
+    end
   end
 
 
