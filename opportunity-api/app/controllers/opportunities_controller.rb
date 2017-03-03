97f0e0d806b4d3dfa621b673978f96ecfbc13dd3
branquinhoaa <amanda.alves15@gmail..com>
2017-03-03 12:07:10 -0300

    fix comparison for values

--- opportunity-api/app/controllers/opportunities_controller.rb
+++ opportunity-api/app/controllers/opportunities_controller.rb
@@ -50,8 +50,8 @@ class OpportunitiesController < ApplicationController
     lowest_btce = btce.json["btc_usd"]["asks"].min_by{ |order| order[0] } #return value, amount
 
     #build the ask object with the lowest ask value:
-    if lowest_kraken[0] < lowest_btce[0]
-      Ask.new(exchange: 'kraken', value: lowest_kraken[0])
+    if lowest_kraken[0].to_f < lowest_btce[0]
+      Ask.new(exchange: 'kraken', value: lowest_kraken[0].to_f)
     else
       Ask.new(exchange: 'btce', value: lowest_btce[0])
     end
@@ -65,8 +65,8 @@ class OpportunitiesController < ApplicationController
     higher_btce = btce.json["btc_usd"]["bids"].max_by{ |order| order[0] } #return value, amount
 
     #build the bid object with the highest bid value
-    if higher_kraken[0] > higher_btce[0]
-      Bid.new(exchange: 'kraken', value: higher_kraken[0])
+    if higher_kraken[0].to_f > higher_btce[0]
+      Bid.new(exchange: 'kraken', value: higher_kraken[0].to_f)
     else
       Bid.new(exchange: 'btce', value: higher_btce[0])
     end
